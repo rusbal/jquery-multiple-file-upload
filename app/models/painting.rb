72a62cd19647750c0b53fd4542d056f949269ec5
Raymond Usbal <rsusbal@philippinedev.com>
2016-05-06 10:52:25 +0800

    styles

--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,12 +1,11 @@
 class Painting < ActiveRecord::Base
-  # before_create :default_name
+  before_create :default_name
 
-  # validates :name, presence: true
   validates :image, presence: true
 
   mount_uploader :image, ImageUploader
 
-  # def default_name
-  #   self.name ||= File.basename(image.filename, '.*').titleize if image
-  # end
+  def default_name
+    self.name ||= File.basename(image.filename, '.*').titleize if image
+  end
 end
